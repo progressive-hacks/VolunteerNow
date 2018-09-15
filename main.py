cca19872631aa57ae3fc96fb14aa4acbaa991278
chiayolin <chiayo.lin@gmail.com>
2018-09-15 04:34:30 -0400

    hand over to Autumn

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import os
 import webapp2
 import jinja2
+import helpers
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 
 JINJA_ENV = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions = ['jinja2.ext.autoescape'],
-    autoescape =True
+    autoescape = True
 )
 
+"""
+class Event(ndb.Model):
+    user = ndb.StringProperty()
+    name = ndb.StringProperty()
+    start_time = ndb.DateTimeProperty()
+    end_time = ndb.DateTimeProperty()
+    description = ndb.TextProperty()
+    geolocation = ndb.GeoPtProperty()
+"""
 
 class HomePage(webapp2.RequestHandler):
     
     def get(self):
-        homepage_template = JINJA_ENV.get_template('templates/homepage.html')
-        self.response.write(homepage_template.render(content="hello, world"))
+        content = JINJA_ENV.get_template('templates/homepage.html')
+        user = users.get_current_user()
+        if user:
+            start_link = "/volunteer"
+        else:
+            start_link = users.create_login_url('/volunteer')
 
+        self.response.write(content.render(content=start_link))
 
-class CalenderPage(webapp2.RequestHandler):
+class CalenderView(webapp2.RequestHandler):
 
     def get(self):
-        calendar_template = JINJA_ENV.get_template("templates/divsForCalendar.html")
-        self.response.write(calendar_template.render())
+        content = JINJA_ENV.get_template("templates/divsForCalendar.html")
+        self.response.write(content.render())
 
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
-    ('/calendar',CalenderPage)
+    ('/volunteer', CalenderView)
 ], debug=True)
