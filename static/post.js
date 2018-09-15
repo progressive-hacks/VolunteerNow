e5c37280795e8f8ae0bdf60882688b303bf9698b
chiayolin <chiayo.lin@gmail.com>
2018-09-15 07:13:11 -0400

    add post

---
+++ static/post.js
@@ -0,0 +1,19 @@
+function post_event(event_id) {
+  $.ajax({
+    url:"/volunteer",
+    method:"POST",
+
+    data: {
+        event: event_id,
+    },
+    
+    success: (response) => {
+        $("#result").html(response);
+      },
+
+    error: () => {
+        $("#result").html("error");
+    }
+});
+
+}
