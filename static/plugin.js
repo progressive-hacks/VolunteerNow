27895283317d5ae013ed5b4303a21e159633c01c
chiayolin <chiayo.lin@gmail.com>
2018-09-16 07:42:01 -0400

    fix redirection

--- static/plugin.js
+++ static/plugin.js
@@ -17,15 +17,16 @@ function register_event(event_id) {
 }
 
 function data_handler(doc, data) {
+    console.log(data)
     var index = 0;
     while(index != data.length) {
         start_time_id = data[index][0];
         final_time_id = data[index][1];
-        event_name    = data[index][3];
-
+        event_name    = data[index][2];
 
         console.log(start_time_id);
-        doc.getElementById(start_time_id).style.background = "#000";
+        doc.getElementById(start_time_id).innerHTML = event_name;
+        doc.getElementById(start_time_id).style.background = "green";
 
         index += 1;
 
